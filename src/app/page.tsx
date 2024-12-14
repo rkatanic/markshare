3d1340d3a1cb1a81ddc550efa02509cb7a9c981e
rkatanic <rkatanic@outlook.com>
2024-12-14 16:12:42 +0100

    Add about and new tab

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,11 @@ import classNames from "classnames";
 
 export default function Home() {
   const [content, setContent] = useQueryState("content");
+  const [mode, setMode] = useQueryState("m");
   const [markdown, setMarkdown] = useState(decodeContent(content ?? ""));
-  const [isEditMode, setIsEditMode] = useState(false);
+  const [isEditMode, setIsEditMode] = useState(
+    mode === "edit" || !markdown ? true : false
+  );
 
   const onMarkdownChange = (value: string): void => {
     setMarkdown(value);
@@ -24,7 +27,10 @@ export default function Home() {
           <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur ">
             <li>
               <button
-                onClick={() => setIsEditMode(true)}
+                onClick={() => {
+                  setIsEditMode(true);
+                  setMode("edit");
+                }}
                 className={classNames(
                   "flex items-center relative px-3 py-2 transition gap-2",
                   {
@@ -53,7 +59,10 @@ export default function Home() {
             </li>
             <li>
               <button
-                onClick={() => setIsEditMode(false)}
+                onClick={() => {
+                  setIsEditMode(false);
+                  setMode(null);
+                }}
                 className={classNames(
                   "flex items-center relative px-3 py-2 transition gap-2",
                   {
@@ -85,6 +94,53 @@ export default function Home() {
                 Preview
               </button>
             </li>
+
+            <li>
+              <a
+                className="flex items-center relative px-3 py-2 transition gap-2"
+                href="/"
+                target="_blank"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-4"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m3.75 9v6m3-3H9m1.5-12H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
+                  />
+                </svg>
+                New
+              </a>
+            </li>
+            <li>
+              <a
+                className="flex items-center relative px-3 py-2 transition gap-2"
+                href="http://localhost:3000/?content=IyMgQWJvdXQKCldlbGNvbWUgdG8gKipNYXJrZG93biBTaGFyZSoqIOKAkyBhIGxpZ2h0d2VpZ2h0IGFuZCBzaW1wbGUgYXBwIGRlc2lnbmVkIHRvIGNyZWF0ZSwgZWRpdCwgYW5kIHNoYXJlIG1hcmtkb3duIGNvbnRlbnQgZWZmb3J0bGVzc2x5LgpNYWRlIGJ5IFtya2F0YW5pY10oaHR0cHM6Ly9naXRodWIuY29tL3JrYXRhbmljKS4KCiMjIyBLZXkgRmVhdHVyZXMKCi0gKipFZGl0IE1hcmtkb3duIHdpdGggRWFzZSoqOiBBIHNpbXBsZSBtYXJrZG93biBlZGl0b3IgdG8gY29tcG9zZSBhbmQgcHJldmlldyB5b3VyIGNvbnRlbnQgaW4gcmVhbC10aW1lLgotICoqVVJMLUJhc2VkIFNoYXJpbmcqKjogWW91ciBjb250ZW50IGlzIGVuY29kZWQgZGlyZWN0bHkgaW50byB0aGUgVVJMLCBtYWtpbmcgc2hhcmluZyBzZWFtbGVzcyBhbmQgcHJpdmF0ZS4KCiMjIyBIb3cgSXQgV29ya3MKCjEuIFdyaXRlIHlvdXIgbWFya2Rvd24gY29udGVudCBpbiB0aGUgZWRpdG9yLgoyLiBUaGUgYXBwIGVuY29kZXMgeW91ciBjb250ZW50IGludG8gdGhlIFVSTCBkeW5hbWljYWxseS4KMy4gU2hhcmUgdGhlIFVSTCB3aXRoIGFueW9uZSB0byBsZXQgdGhlbSB2aWV3IG9yIGVkaXQgdGhlIGNvbnRlbnQuCgojIyMgV2h5IHRoaXM/CgpNYXJrZG93biBTaGFyZSBpcyBidWlsdCBmb3IgKipkZXZlbG9wZXJzLCB3cml0ZXJzLCBhbmQgY29sbGFib3JhdG9ycyoqIHdobyB2YWx1ZSBzaW1wbGljaXR5LCBwcml2YWN5LCBhbmQgZWZmaWNpZW5jeS4gV2hldGhlciBpdCdzIHNoYXJpbmcgYSBxdWljayBub3RlLCBhIHRlY2huaWNhbCBkb2N1bWVudCwgb3IgZHJhZnQgY29udGVudCwgTWFya2Rvd24gU2hhcmUga2VlcHMgaXQgZmFzdCBhbmQgc2VjdXJlLgoKLS0tCgoqKkJ1aWx0IHdpdGgg4p2k77iPIGFuZCBzaW1wbGljaXR5IGluIG1pbmQuKioK"
+                target="_blank"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-4"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z"
+                  />
+                </svg>
+                About
+              </a>
+            </li>
           </ul>
         </nav>
       </div>
@@ -97,12 +153,13 @@ export default function Home() {
           }}
           name="markdown"
           id="markdown"
+          placeholder="Write your markdown here..."
           value={markdown}
           onChange={(e) => onMarkdownChange(e.target.value)}
         />
       ) : (
         <Markdown className="prose p-8 w-full max-w-2xl !flex-1 sm:prose-lg py-24 mx-auto prose-headings:tracking-tight prose-h1:font-bold prose-h1:tracking-tight">
-          {markdown}
+          {markdown ? markdown : "Nothing to preview yet."}
         </Markdown>
       )}
     </div>
